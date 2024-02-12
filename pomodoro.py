1397324589bd7f3dde1287306b214f0b0d5cef89
jm989 <145062461+jm989@users.noreply.github.com>
2024-02-12 10:42:58 +0000

    Need to rework this, need to give the users the option to pick from a range of times rather than letting them set their own times

--- pomodoro.py
+++ pomodoro.py
@@ -1,1 +1,10 @@
-print("Hello World")
+work = int(input("Enter work time: "))
+rest = int(input("Enter rest time: "))
+
+def check_rest():
+    if rest != 5 or rest != 10 != 15:
+        print("Rest times must either be 5, 10 or 15 minutes")
+
+def checkWork():
+    if work % 5 != 0:
+        print("")
